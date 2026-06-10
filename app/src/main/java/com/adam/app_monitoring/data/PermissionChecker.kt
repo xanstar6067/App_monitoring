6d1d7d9e3e83949fb55bad72c62bcdd5c9b20966
xanstar6067 <district6067@gmail.com>
2026-06-10 12:41:18 +0300

    Исправление АПИ проверки. Адаптация для старых версий АПИ. Минимальный АПИ 28 (Андроид 9)

--- app/src/main/java/com/adam/app_monitoring/data/PermissionChecker.kt
+++ app/src/main/java/com/adam/app_monitoring/data/PermissionChecker.kt
@@ -20,11 +20,19 @@ class PermissionChecker(private val context: Context) {
 
     fun hasUsageAccess(): Boolean {
         val appOps = context.getSystemService(AppOpsManager::class.java)
-        val mode = appOps.unsafeCheckOpNoThrow(
-            AppOpsManager.OPSTR_GET_USAGE_STATS,
-            Process.myUid(),
-            context.packageName
-        )
+        val mode = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            appOps.unsafeCheckOpNoThrow(
+                AppOpsManager.OPSTR_GET_USAGE_STATS,
+                Process.myUid(),
+                context.packageName
+            )
+        } else {
+            appOps.checkOpNoThrow(
+                AppOpsManager.OPSTR_GET_USAGE_STATS,
+                Process.myUid(),
+                context.packageName
+            )
+        }
         return mode == AppOpsManager.MODE_ALLOWED
     }
 
