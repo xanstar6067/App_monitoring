6e548483c0a561a87cc3549dcf7cee4d5f1407b8
xanstar6067 <district6067@gmail.com>
2026-06-10 12:04:06 +0300

    Настроил целевой SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.adam.app_monitoring"
-        minSdk = 30
+        minSdk = 28
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
